e8c0103cf34debb8f1b06973167562c2b344f88b
Thu Hoai <030239230057@st.buh.edu.vn>
2026-04-19 12:42:49 +0700

    cập nhật trang quản lý sách và thể loại

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SachController4;
 
 Route::get('/', [HomeController::class, 'index']);
 
@@ -16,3 +17,38 @@ Route::middleware(['auth', 'admin'])->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+
+Route::prefix('admin')->group(function () {
+    
+    // --- THÊM SÁCH ---
+    Route::get('/themsach', [SachController4::class, 'trangThemSach'])->name('admin.themsach');
+    Route::post('/themsach', [SachController4::class, 'luuSach'])->name('admin.themsach.submit');
+
+    // --- SỬA SÁCH (Đường dẫn này sẽ giải quyết cái lỗi đỏ lòm lúc nãy) ---
+    Route::get('/sach/edit/{id}', [SachController4::class, 'trangSuaSach'])->name('admin.sach.edit');
+    Route::post('/sach/edit/{id}', [SachController4::class, 'capNhatSach'])->name('admin.sach.update');
+
+    // --- XÓA SÁCH ---
+    Route::get('/sach/delete/{id}', [SachController4::class, 'xoaMemSach'])->name('admin.sach.delete');
+    
+    // --- DANH SÁCH & LỌC THỂ LOẠI ---
+    // QUAN TRỌNG: Route có đuôi {id_theloai?} luôn phải nằm ở ĐÁY của group này
+    Route::get('/theloai', [SachController4::class, 'trangQuanLyTheLoai'])->name('admin.theloai');
+
+   
+    Route::get('/sach/{id_theloai?}', [SachController4::class, 'trangQuanLySach'])->name('admin.sach');
+
+    // Thêm thể loại
+    Route::get('/theloai/create', [SachController4::class, 'trangThemTheLoai'])->name('admin.theloai.create');
+    Route::post('/theloai/store', [SachController4::class, 'luuTheLoai'])->name('admin.theloai.store');
+    
+    // Sửa thể loại
+    Route::get('/theloai/edit/{id}', [SachController4::class, 'trangSuaTheLoai'])->name('admin.theloai.edit');
+    Route::post('/theloai/update/{id}', [SachController4::class, 'capNhatTheLoai'])->name('admin.theloai.update');
+    
+    // Xóa (mềm) thể loại
+    Route::get('/theloai/delete/{id}', [SachController4::class, 'xoaTheLoai'])->name('admin.theloai.delete');
+
+});
