5d42512d4438e2679d03dbf0dacb2cfbccdd97e4
nthaong246 <030239230220@st.buh.edu.vn>
2026-04-19 12:56:13 +0700

    nhuthao

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,19 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BookController;
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/index', function () {
     return view('sach.index');
 })->middleware(['auth', 'verified'])->name('index');
 
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin/index', function () {
-        return view('admin.index');
-    })->name('admin.index');
+Route::get('/chitiet/{id}', [BookController::class, 'chitietsach'])->name('sach.details');
+Route::post('/them-gio-hang', [BookController::class, 'cartadd'])->name('cartadd');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/giohang', [BookController::class, 'order'])->name('order');
+    Route::post('/cart/delete', [BookController::class, 'cartdelete'])->name('cartdelete');
+    Route::post('/order/create', [BookController::class, 'ordercreate'])->name('ordercreate');
+    Route::post('/sach/danhgia', [BookController::class, 'danhgia'])->name('book.rate');
+    
 });
-
-require __DIR__.'/auth.php';
